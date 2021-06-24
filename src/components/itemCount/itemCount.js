32de258503e7c484f53e80f79beb4479da4de394
Diego <diego.ullua@outlook.com>
2021-06-24 20:02:58 -0300

    realize los cambios sugeridos por Flavio

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -1,12 +1,16 @@
-import React ,{useState} from 'react';
+import React ,{useState,useEffect} from 'react';
 import './itemCount.css';
 
-const ItemCount = ({nombreProducto, stock,setStock,initialValue,setInitialValue,onAdd}) => {
+const ItemCount = ({nombreProducto,initialValue,onAdd}) => {
+    
+    const [value, setValue] = useState(initialValue);
+    const [stock, setStock] = useState(10);
+    const [disabled, setDisabled] = useState(false);
 
     const restarCantidad = () =>{
 
-        if(initialValue > 1 ){
-            setInitialValue(initialValue-1)
+        if(value > 1 ){
+            setValue(value-1)
             setStock(stock+1)
             
         }
@@ -15,23 +19,40 @@ const ItemCount = ({nombreProducto, stock,setStock,initialValue,setInitialValue,
 
     const agregarCantidad = () =>{
 
-        if(stock > 0){
-            setInitialValue(initialValue+1)
+        if(stock >= 1){
+            setValue(value+1)
             setStock(stock-1)
-           
         }
 
     }
+
+    useEffect(() => {
+
+        const onInit = (stock) => {
+
+            if(stock === 0){
+                setDisabled(true)
+                setValue(0)
+            }
+            setStock(stock - value)
+        }
+        
+        onInit(stock)
+   
+    }, [])
+
+
     return ( 
         <div className="itemCountContainer">
             <label>{nombreProducto}</label>
-            <small>Stock: {stock}</small>
+            {stock > 0 ? <small>Disponibles: {stock}</small> : <small>No hay mas stock disponible</small>}
+            
             <div className="inputContainer">
                 <button className="inputBtn" onClick={restarCantidad}>-</button>
-                <input type="text" value={initialValue}/>
+                <input type="text" value={value}/>
                 <button className="inputBtn" onClick={agregarCantidad}>+</button>
             </div>
-            <button className="addToCartBtn" onClick={onAdd}>Agregar al carrito</button>
+            <button className="addToCartBtn" onClick={onAdd} disabled={disabled}>Agregar al carrito</button>
 
         </div>
      );
--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -4,16 +4,13 @@ import ItemCount from '../itemCount/itemCount'
 
 const ItemListContainer = ({message}) => {
 
-    const [stock, setStock] = useState(8);
-    const [initialValue, setInitialValue] = useState(2);
+    
+   
     return (  
         <div>
             <p className="itemListMessage">{message}</p>
-            <ItemCount stock={stock} 
-                    nombreProducto="NOTEBOOK" 
-                    initialValue={initialValue}
-                    setStock={setStock}
-                    setInitialValue={setInitialValue}
+            <ItemCount nombreProducto="NOTEBOOK" 
+                    initialValue={1}
                     />
         </div>
     );
